5e7f5519e23a7c78231e471473273b4be27691fb
RSibaja88 <54650378+RSibaja88@users.noreply.github.com>
2020-09-02 22:50:23 -0400

    server set up, endpoints and schema done.

--- server/src/README.md
+++ server/src/README.md
@@ -1,19 +1,11 @@
 Travel Log app using MERN stack.
-*set up server and dependencies
-*set up express and middleware
-*installed Mongoose, created models (Log Entry). I used mongoose because MongoDB doesnt have schemas
-*created schema
 
-Designing Database:
-Log Entry
--Title - text
--Description -text
--Comments? - text
--Rating (scale 1-10) -num
--Image - text -url
--Start date - dateTime
--End date - dateTime
--Latitude - num
--Longitude - num
--Created at - dateTime
--Updated at - dateTime
+-set up server and dependencies
+
+-set up express and middleware
+
+-installed Mongoose, created models (Log Entry). I used mongoose because MongoDB doesnt have schemas
+
+-created schema with mongoose
+
+-created GET, GET by ID, POST, and DELETE endpoint routes for the Express API and tested in Postman
--- server/src/index.js
+++ server/src/index.js
@@ -2,14 +2,27 @@ const express = require("express");
 const morgan = require("morgan");
 const helmet = require("helmet");
 const cors = require("cors");
-// const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const middlewares = require("./middlewares");
+const logs = require("./api/logs");
 
 const app = express();
+
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
 app.use(morgan("common"));
 app.use(helmet());
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+  })
+);
+app.use(express.json()); //body parsing mw
 
 app.get("/", (req, res) => {
   res.json({
@@ -17,6 +30,7 @@ app.get("/", (req, res) => {
   });
 });
 
+app.use("/api/logs", logs);
 app.use(middlewares.notFound);
 app.use(middlewares.errorHandler);
 
