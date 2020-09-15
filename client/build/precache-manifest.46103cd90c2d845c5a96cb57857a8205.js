a7ef60ccffa04d35a8e3e66c38629eac5c082112
RSibaja88 <54650378+RSibaja88@users.noreply.github.com>
2020-09-15 00:15:28 -0400

    took build off gitignore

---
+++ client/build/precache-manifest.46103cd90c2d845c5a96cb57857a8205.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3cce9764fe7c4fa7933c463c1a6a9bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a5b6ba6c55b2e3a2cda",
+    "url": "/static/css/main.2b42e405.chunk.css"
+  },
+  {
+    "revision": "3bd73f07c84b2052f58d",
+    "url": "/static/js/2.bbc9e1b2.chunk.js"
+  },
+  {
+    "revision": "61eedd39499d34d7aaf09803c460ef46",
+    "url": "/static/js/2.bbc9e1b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a5b6ba6c55b2e3a2cda",
+    "url": "/static/js/main.00a62c29.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "ffeb15ed0e9142cf21dd3e35a376ebd5",
+    "url": "/static/media/PettingillCF-Bold.ffeb15ed.otf"
+  },
+  {
+    "revision": "13be9a8361028e9c941765e7a2d06426",
+    "url": "/static/media/YanoneKaffeesatz-Bold.13be9a83.otf"
+  },
+  {
+    "revision": "f607fb5908cd4cd313a5459de23ea918",
+    "url": "/static/media/YanoneKaffeesatz-Light.f607fb59.otf"
+  },
+  {
+    "revision": "62b6a7056e853edd8e5f55fd622a1896",
+    "url": "/static/media/YanoneKaffeesatz-Regular.62b6a705.otf"
+  },
+  {
+    "revision": "403f9cfd8bee96919c1927cd084e47fa",
+    "url": "/static/media/bronze-metal-textureDeep.403f9cfd.jpg"
+  },
+  {
+    "revision": "80d956ebddebcf2c2e91b58caa4a785d",
+    "url": "/static/media/goldWhereWorld.80d956eb.png"
+  },
+  {
+    "revision": "775c91f92fa415c585485df32cf45b12",
+    "url": "/static/media/leathertile.775c91f9.png"
+  }
+]);
