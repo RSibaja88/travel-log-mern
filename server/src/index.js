d28d7bec7c5ade6cecd84db95d13d782bf4dce25
RSibaja88 <54650378+RSibaja88@users.noreply.github.com>
2020-09-23 21:14:56 -0400

    .

--- server/src/index.js
+++ server/src/index.js
@@ -3,7 +3,9 @@ const morgan = require("morgan");
 const helmet = require("helmet");
 const cors = require("cors");
 const mongoose = require("mongoose");
-require("dotenv").config();
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const middlewares = require("./middlewares");
 const logs = require("./api/logs");
